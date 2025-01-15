0c44bed4b9427d5c44a07bdc2b95bf1eaa73c486
조수빈 <whtnqls03@gmail.com>
2025-01-15 23:02:43 +0900

    Spring Interceptor

--- src/main/java/com/example/websample/config/WebConfig.java
+++ src/main/java/com/example/websample/config/WebConfig.java
@@ -4,9 +4,11 @@ import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
     @Bean
     public FilterRegistrationBean loggingFilter() {
@@ -16,5 +18,13 @@ public class WebConfig {
         filterRegistrationBean.addUrlPatterns("/*");
 
         return filterRegistrationBean;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/*", "/images/*");
     }
 }
--- src/main/java/com/example/websample/controller/SampleController.java
+++ src/main/java/com/example/websample/controller/SampleController.java
@@ -9,8 +9,13 @@ import org.springframework.web.bind.annotation.*;
 public class SampleController {
 
     @GetMapping(value = "/order/{orderId}")
-    public String getOrder(@PathVariable("orderId") String id) {
+    public String getOrder(@PathVariable("orderId") String id)
+            throws IllegalAccessException {
         log.info("Get some order : " + id);
+
+        if ("500".equals(id)) {
+            throw new IllegalAccessException("500 is not valid orderId.");
+        }
         return "orderId:" + id + "," + "orderAmount: 1000";
     }
 
@@ -25,7 +30,7 @@ public class SampleController {
             @RequestParam(value = "orderId", required = false,
                     defaultValue = "defaultId") String id,
             @RequestParam("orderAmount") Integer amount) {
-        log.info("Get some order : " + id + ", amount : " + amount);
+        log.info("Get order : " + id + ", amount : " + amount);
         return "orderId:" + id + "," + "orderAmount:" + amount;
     }
 
