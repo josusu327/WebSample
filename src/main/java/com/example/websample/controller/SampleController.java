5c12e4bb4aa38a2e71067e91f07e3fcd7d9ce503
조수빈 <whtnqls03@gmail.com>
2025-01-15 19:54:29 +0900

    Spring HTTP ex3

--- src/main/java/com/example/websample/controller/SampleController.java
+++ src/main/java/com/example/websample/controller/SampleController.java
@@ -1,25 +1,54 @@
 package com.example.websample.controller;
 
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
 public class SampleController {
 
-    @GetMapping(value = "/order/1")
-    public String getOrder(){
-        log.info("Get some order");
-        return "orderId: 1 orderAmount: 1000";
+    @GetMapping(value = "/order/{orderId}")
+    public String getOrder(@PathVariable("orderId") String id) {
+        log.info("Get some order : " + id);
+        return "orderId:" + id + "," + "orderAmount: 1000";
+    }
+
+    @DeleteMapping(value = "/order/{orderId}")
+    public String deleteOrder(@PathVariable("orderId") String id) {
+        log.info("Delete some order : " + id);
+        return "Delete orderId:" + id;
+    }
+
+    @GetMapping(value = "/order")
+    public String getOrderWithRequestParam(
+            @RequestParam(value = "orderId", required = false,
+                    defaultValue = "defaultId") String id,
+            @RequestParam("orderAmount") Integer amount) {
+        log.info("Get some order : " + id + ", amount : " + amount);
+        return "orderId:" + id + "," + "orderAmount:" + amount;
+    }
+
+    @PostMapping(value = "/order")
+    public String createOrder(
+            @RequestBody CreateOrderRequest createOrderRequest,
+            @RequestHeader String userAccountId) {
+        log.info("Create order : " + createOrderRequest +
+                ", userAccountId : " + userAccountId);
+        return "orderId:" + createOrderRequest.getOrderId() + ", " +
+                "orderAmount:" + createOrderRequest.getOrderAmount();
     }
 
     @PutMapping(value = "/order")
-    public String createOrder(){
+    public String createOrder() {
         log.info("Create order");
         return "order created -> orderId: 1 orderAmount: 1000";
+    }
+
+    @Data
+    public static class CreateOrderRequest {
+        private String orderId;
+        private Integer orderAmount;
     }
 
 }
