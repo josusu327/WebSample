32c324140708a67c98e0454935f3efe8ae19f163
조수빈 <whtnqls03@gmail.com>
2025-01-16 00:35:08 +0900

    Spring Exception ex3 @RestControllerAdvice 활용

--- src/main/java/com/example/websample/controller/SampleController.java
+++ src/main/java/com/example/websample/controller/SampleController.java
@@ -1,12 +1,9 @@
 package com.example.websample.controller;
 
-import com.example.websample.dto.ErrorResponse;
 import com.example.websample.exception.ErrorCode;
 import com.example.websample.exception.WebSampleException;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLIntegrityConstraintViolationException;
@@ -43,39 +40,6 @@ public class SampleController {
         }
 
         return "orderId:" + id + "," + "orderAmount: 1000";
-    }
-
-    @ExceptionHandler(IllegalAccessException.class)
-    public ResponseEntity<ErrorResponse> handleIllegalAccessException(
-            IllegalAccessException e) {
-        log.error("IllegalAccessException is occurred", e);
-
-        return ResponseEntity
-                .status(HttpStatus.FORBIDDEN)
-                .body(new ErrorResponse(ErrorCode.TOO_BIG_ID_ERROR,
-                        "IllegalAccessException is occurred"));
-    }
-
-    @ExceptionHandler(WebSampleException.class)
-    public ResponseEntity<ErrorResponse> handleWebSampleException(
-            WebSampleException e) {
-        log.error("WebSampleException is occurred", e);
-
-        return ResponseEntity
-                .status(HttpStatus.INSUFFICIENT_STORAGE)
-                .body(new ErrorResponse(e.getErrorCode(),
-                        "WebSampleException is occurred"));
-    }
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleException(
-            Exception e) {
-        log.error("Exception is occurred", e);
-
-        return ResponseEntity
-                .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(new ErrorResponse(ErrorCode.INTERNAL_SERVER_ERROR,
-                        "Exception is occurred"));
     }
 
     @DeleteMapping(value = "/order/{orderId}")
